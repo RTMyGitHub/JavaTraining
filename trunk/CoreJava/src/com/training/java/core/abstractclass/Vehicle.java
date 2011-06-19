dec253a59d4488d561f5f09e3921850f351bcdb6
venkatmail@gmail.com <venkatmail@gmail.com@ff6a12cf-7b8c-2e6c-7a3d-b3d363c5d2a6>
2011-06-19 14:56:51 +0000

    Initial import..

---
+++ trunk/CoreJava/src/com/training/java/core/abstractclass/Vehicle.java
@@ -0,0 +1,26 @@
+package com.training.java.core.abstractclass;
+
+public abstract class Vehicle
+{
+	/**
+	 * This is a concrete method.
+	 */
+	public void start()
+	{
+		System.out.println("Vehicle is starting");
+	}
+
+	/**
+	 * This is a concrete method.
+	 */
+	public void stop()
+	{
+		System.out.println("Vehicle is stopping");
+	}
+
+	/**
+	 * Since we think every vehicle drives differently, we make it abstract.
+	 */
+	public abstract void drive();
+
+}
